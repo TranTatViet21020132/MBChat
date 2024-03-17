c5404b58e3969e5fe77f6a8dd4ee773646531d34
TrieuThanhTung <tung.ttrieu@gmail.com>
2024-03-18 00:27:47 +0700

    add items settings

--- MBChat.FE/app/(tabs)/settings/index.tsx
+++ MBChat.FE/app/(tabs)/settings/index.tsx
@@ -1,8 +1,9 @@
-import { View, Text, StyleSheet, ScrollView, Image } from 'react-native'
+import { View, Text, StyleSheet, ScrollView, Image, Pressable, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { COLORS } from '@/constants/Colors'
 import { SearchBar } from 'react-native-elements'
 import { useState } from 'react'
+import { MaterialCommunityIcons, Ionicons, Fontisto, Octicons } from '@expo/vector-icons'
 
 
 const Settings = () => {
@@ -14,6 +15,8 @@ const Settings = () => {
       style={styles.container}
     >
       <Header />
+      <UserItems />
+      <AppItems />
     </ScrollView>
   )
 }
@@ -58,26 +61,153 @@ const Header = () => {
           </Text>
         </View>
       </View>
+      <TouchableOpacity style={styles.item}>
+          <View style={styles.itemIcon}>
+            <MaterialCommunityIcons name='camera-plus-outline' size={24} color={COLORS.light.primary}/>
+          </View>
+          <View style={styles.itemTitle}>
+            <Text style={{fontSize: 16, color: COLORS.light.primary}}>
+              Set Profile Photo
+            </Text>
+            <MaterialCommunityIcons name='chevron-right' size={24} color={COLORS.light.text}/>
+          </View>
+      </TouchableOpacity>
     </View>
   )
 }
 
+const UserItems = () => {
+
+  return (
+    <View style={styles.items}>
+      <TouchableOpacity style={styles.item}>
+          <View style={styles.itemIcon}>
+            <Ionicons name='key-outline' size={24} color={COLORS.light.primary}/>
+          </View>
+          <View style={styles.itemTitle}>
+            <Text style={{fontSize: 16, color: COLORS.light.text}}>
+              Account
+            </Text>
+            <MaterialCommunityIcons name='chevron-right' size={24} color={COLORS.light.text}/>
+          </View>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.item}>
+          <View style={styles.itemIcon}>
+            <MaterialCommunityIcons name='devices' size={24} color={COLORS.light.primary}/>
+          </View>
+          <View style={styles.itemTitle}>
+            <Text style={{fontSize: 16, color: COLORS.light.text}}>
+              Devices
+            </Text>
+            <MaterialCommunityIcons name='chevron-right' size={24} color={COLORS.light.text}/>
+          </View>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.item}>
+          <View style={styles.itemIcon}>
+            <Ionicons name='notifications-outline' size={24} color={COLORS.light.primary}/>
+          </View>
+          <View style={styles.itemTitle}>
+            <Text style={{fontSize: 16, color: COLORS.light.text}}>
+              Notifications
+            </Text>
+            <MaterialCommunityIcons name='chevron-right' size={24} color={COLORS.light.text}/>
+          </View>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.item}>
+          <View style={styles.itemIcon}>
+            <Fontisto name='world-o' size={24} color={COLORS.light.primary}/>
+          </View>
+          <View style={styles.itemTitle}>
+            <Text style={{fontSize: 16, color: COLORS.light.text}}>
+              Language
+            </Text>
+            <MaterialCommunityIcons name='chevron-right' size={24} color={COLORS.light.text}/>
+          </View>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const AppItems = () => {
+
+  return (
+    <View style={styles.items}>
+      <TouchableOpacity style={styles.item}>
+          <View style={styles.itemIcon}>
+            <Ionicons name='lock-closed-outline' size={24} color={COLORS.light.primary}/>
+          </View>
+          <View style={styles.itemTitle}>
+            <Text style={{fontSize: 16, color: COLORS.light.text}}>
+              Privacy And Security
+            </Text>
+            <MaterialCommunityIcons name='chevron-right' size={24} color={COLORS.light.text}/>
+          </View>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.item}>
+          <View style={styles.itemIcon}>
+            <Octicons name='question' size={24} color={COLORS.light.primary}/>
+          </View>
+          <View style={styles.itemTitle}>
+            <Text style={{fontSize: 16, color: COLORS.light.text}}>
+              App FAQ
+            </Text>
+            <MaterialCommunityIcons name='chevron-right' size={24} color={COLORS.light.text}/>
+          </View>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.item}>
+          <View style={styles.itemIcon}>
+            <MaterialCommunityIcons name='lightbulb-outline' size={24} color={COLORS.light.primary}/>
+          </View>
+          <View style={styles.itemTitle}>
+            <Text style={{fontSize: 16, color: COLORS.light.text}}>
+              App Features
+            </Text>
+            <MaterialCommunityIcons name='chevron-right' size={24} color={COLORS.light.text}/>
+          </View>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
 const styles = StyleSheet.create({
   container: {
-    marginRight: 20,
-    marginLeft: 20,
-    marginTop: 20,
+   paddingRight: 20,
+   paddingLeft: 20,
+   paddingTop: 20,
   },
   header: {
     backgroundColor: '#fff',
     height: 140,
-    borderRadius: 10
+    borderRadius: 10,
+    marginBottom: 28
   },
   avatar: {
-    height: 100,
+    // height: 100,
+    flex: 1,
     borderBottomWidth: 1,
     borderBottomColor: COLORS.light.settings.backgroudColor,
     flexDirection: 'row'
+  },
+  item: {
+    height: 40,
+    alignItems: 'center',
+    flexDirection: 'row',
+    borderBottomWidth: 1,
+    borderBottomColor: COLORS.light.settings.backgroudColor,
+  },
+  itemIcon: {
+    marginLeft: 20,
+    marginRight: 20
+  },
+  itemTitle: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  items: {
+    marginBottom: 28,
+    borderRadius: 10,
+    backgroundColor: '#fff'
   }
 })
 
