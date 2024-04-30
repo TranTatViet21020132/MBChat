a411344448455d9ce448904fb2dab1db911a0965
phamtungthuy <94853261+phamtungthuy@users.noreply.github.com>
2024-04-30 21:42:14 +0700

    Install webrtc and expo-client-dev to build expo

--- MBChat.FE/app/(tabs)/calls.tsx
+++ MBChat.FE/app/(tabs)/calls.tsx
@@ -1,12 +1,22 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-
+import { StyleSheet } from 'react-native'
+import Button from '@/components/VideoCall/Button'
 const Calls = () => {
   return (
-    <View>
-      <Text>Calls</Text>
+    <View style={styles.container}>
+      <Button iconName="video" backgroundColor="grey" />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  }
+})
 
 export default Calls
