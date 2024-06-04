ec7c7fd51d27dd6016a800e6349054d8828dfc6b
phamtungthuy <94853261+phamtungthuy@users.noreply.github.com>
2024-06-04 22:36:00 +0700

    Add reactions in chat of communities

--- MBChat.FE/constants/ApiUrl.ts
+++ MBChat.FE/constants/ApiUrl.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = "https://5505-116-96-44-191.ngrok-free.app/"
-export const BASE_DOMAIN = "5505-116-96-44-191.ngrok-free.app" 
+export const BASE_URL = "https://01aa-116-96-44-191.ngrok-free.app/"
+export const BASE_DOMAIN = "01aa-116-96-44-191.ngrok-free.app" 
