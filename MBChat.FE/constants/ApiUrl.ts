27fbd25c4c8ad6a9bc4062aa9817ee51e4abb4f1
TrieuThanhTung <tung.ttrieu@gmail.com>
2024-06-08 23:37:10 +0700

    add search message

--- MBChat.FE/constants/ApiUrl.ts
+++ MBChat.FE/constants/ApiUrl.ts
@@ -1,2 +1,5 @@
-export const BASE_URL = "https://4ac3-116-96-44-191.ngrok-free.app/"
-export const BASE_DOMAIN = "4ac3-116-96-44-191.ngrok-free.app" 
+// export const BASE_URL = "https://4ac3-116-96-44-191.ngrok-free.app/"
+// export const BASE_DOMAIN = "4ac3-116-96-44-191.ngrok-free.app" 
+
+export const BASE_URL = "http://112.137.129.161:8001/"
+export const BASE_DOMAIN = "112.137.129.161:8001" 
